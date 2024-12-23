7075a0070f3a783ec1d6d792bffac3f696ba7d65
cristianjvj <cristianjaureguiv@gmail.com>
2024-12-22 21:01:48 -0300

    Add methods to the CommonPageMethods class

--- pages/common-page/common-page.methods.ts
+++ pages/common-page/common-page.methods.ts
@@ -1,11 +1,35 @@
 import {Page} from 'playwright'
+import { CommonPageElements } from './common-page.elements'
+
 export class CommonPageMethods {
     private page: Page
+    private commonPageElements: CommonPageElements
+
     constructor(page) {
         this.page = page
+        this.commonPageElements = new CommonPageElements(page)
     }
 
     async navigateToTheApplication() {
         await this.page.goto('https://www.saucedemo.com/v1//index.html')
+    }
+    async openMenu() {
+        await this.commonPageElements.buttons.openMenu.click()
+    }
+
+    async clickOnAllItemsOption() {
+        await this.commonPageElements.leftMenu.allItems.click()
+    }
+
+    async clickOnAboutOption() {
+        await this.commonPageElements.leftMenu.about.click()
+    }
+
+    async clickOnLogoutOption() {
+        await this.commonPageElements.leftMenu.logout.click()
+    }
+
+    async clickOnResetAppStateOption() {
+        await this.commonPageElements.leftMenu.resetAppState.click()
     }
 }
