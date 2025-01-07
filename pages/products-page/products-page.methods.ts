419e4d1398f867108649546e2d1cf2d39a99de18
cristianjvj <cristianjaureguiv@gmail.com>
2025-01-06 22:58:14 -0300

    Completing test suit 'Login with valid credentials'

--- pages/products-page/products-page.methods.ts
+++ pages/products-page/products-page.methods.ts
@@ -1,6 +1,7 @@
 import { Page }from 'playwright'
 import { ProductsPageElements } from './products-page.elements';
 import { Logger } from '../../support/logger';
+import { expect } from '@playwright/test';
 export class ProductsPageMethods {
     private page: Page
     private productsPageElements: ProductsPageElements
@@ -18,5 +19,11 @@ export class ProductsPageMethods {
     async clickOnCartIcon() {
         await Logger.logStep('Click on Cart icon')
         await this.productsPageElements.icons.cart.click()
+    }
+
+    async verifyProducstPageIsDisplayed() {
+        await Logger.logVerification('Verify that Products title is displayed')
+        const elementsCount = await this.productsPageElements.otherElements.pageTitle.count()
+        expect(elementsCount).toEqual(1)
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,27 +3,20 @@ import { CommonPageMethods } from '../pages/common-page/common-page.methods'
 import { LoginPageMethods } from '../pages/login-page/login-page.methods'
 import { LoginPageData } from '../pages/login-page/login-page.data'
 import { ProductsPageMethods } from '../pages/products-page/products-page.methods'
-import { CartPageMethods } from '../pages/cart-page/cart-page.methods'
-import { Logger } from '../support/logger'
 
 const userCredentials = LoginPageData.credentials;
 
-test('Login with valid credentials', async({page}) => {
-    const commonPageMethods = new CommonPageMethods(page)
-    const loginPageMethods = new LoginPageMethods(page)
-    const productsPageMethods = new ProductsPageMethods(page)
-    const cartPageMethods = new CartPageMethods(page)
+test.describe('Login', () => {
 
-    await Logger.logStep('Navigate to the application')
-    await commonPageMethods.navigateToTheApplication()
-    await loginPageMethods.insertUserName(userCredentials.usernames.standardUser)
-    await loginPageMethods.insertPassword(userCredentials.password)
-    await loginPageMethods.clickOnLoginButton()
-    await productsPageMethods.clickOnAddToCart('Sauce Labs Backpack')
-    await productsPageMethods.clickOnCartIcon()
-    await cartPageMethods.clickOnCheckoutButton()
-    //await cartPageMethods.clickOnContinueShoppingButton()
-    //await cartPageMethods.clickOnRemoveButton('Sauce Labs Backpack')
-    // await commonPageMethods.openMenu()
-    await page.waitForTimeout(4000)
-    })
+    test('Login with valid credentials', async({page}) => {
+        const commonPageMethods = new CommonPageMethods(page)
+        const loginPageMethods = new LoginPageMethods(page)
+        const productsPageMethods = new ProductsPageMethods(page)
+
+        await commonPageMethods.navigateToTheApplication()
+        await loginPageMethods.insertUserName(userCredentials.usernames.standardUser)
+        await loginPageMethods.insertPassword(userCredentials.password)
+        await loginPageMethods.clickOnLoginButton()
+        await productsPageMethods.verifyProducstPageIsDisplayed()
+    })    
+})
